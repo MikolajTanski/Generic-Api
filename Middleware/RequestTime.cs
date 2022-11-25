6a5107753c1d35ea1ea1ddfc03decf6a71c21efc
Mikolaj Tanski <mikolajtanski1@gmail.com>
2022-11-25 03:56:50 +0100

    TIme Handler

--- Middleware/RequestTime.cs
+++ Middleware/RequestTime.cs
@@ -1,6 +1,36 @@
-﻿namespace zadanie.Middleware
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace zadanie.Middleware
 {
-    public class Class
+    public class RequestTime : IMiddleware
     {
+        private readonly ILogger<RequestTime> _logger;
+        private Stopwatch _stopWatch;
+
+        public RequestTime(ILogger<RequestTime> logger)
+        {
+            _logger = logger;
+            _stopWatch = new Stopwatch();
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            _stopWatch.Start();
+            await next.Invoke(context);
+            _stopWatch.Stop();
+
+            var elapsedMilliseconds = _stopWatch.ElapsedMilliseconds;
+            if (elapsedMilliseconds / 1000 > 4)
+            {
+                var message =
+                    $"Request [{context.Request.Method}] at {context.Request.Path} took {elapsedMilliseconds} ms";
+
+                _logger.LogInformation(message);
+
+            }
+
+        }
     }
 }
